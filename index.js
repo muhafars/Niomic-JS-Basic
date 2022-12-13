ac9e29d48ae2d30cbff4e8917f3c7a4aefdc4d1b
muhafash <me.muhfarash@gmail.com>
2022-12-14 02:33:02 +0700

    Tugas 2 Done

--- index.js
+++ index.js
@@ -4,12 +4,16 @@
 *? 2. Teman - Teman Buatlah File di Folder javascript teman teman sebuah file dengan nama file tugas1.js
 *? 3. Buatlah Program Dengan Spesifikasi dibawah ini :
 *ToDo
-Buatlah 3 buah variabel menggunakan var , let dan const, Variabel var berisikan nama depan teman teman , variabel let berisikan nama akhir teman teman , const berisikan umur teman teman saat ini, Setelah itu teman teman tampilkan ketiga variabel tersebut dengan console.log()
-*@Hint : number tanpa petik string dengan petik
-*?4. Happy Codding :)
+- Buatlah 5 buah variabel dengan 5 tipe data yang berbeda number , string , boolean , null atau undifined dan array
+- Didalam variabel tersebut teman teman isikan nilai yang berbeda tiap variabelnya
+-Setelah itu teman teman tampilkan kelima variabel tersebut dengan console.log()
+*? 4. Happy Codding :)
  */
 
-var firstName = "Muhammad Fajri";
-let lastName = "Assidiq";
-const age = 25;
-console.log(firstName + lastName + age);
+let number = 1;
+let string = "string";
+let boolean = true;
+let nullVar = null;
+let undifined;
+
+console.log(number, string, boolean, nullVar, undifined);
