3a4a88f0242a4a60f47061ece6e3ec8bfc0c78f5
muhafash <me.muhfarash@gmail.com>
2022-12-14 02:25:36 +0700

    Tugas 1 Done

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ ** Tugas 1
+*? 1. Pastikan Teman Teman Sudah Menginstall Node JS
+*? 2. Teman - Teman Buatlah File di Folder javascript teman teman sebuah file dengan nama file tugas1.js
+*? 3. Buatlah Program Dengan Spesifikasi dibawah ini :
+*ToDo
+Buatlah 3 buah variabel menggunakan var , let dan const, Variabel var berisikan nama depan teman teman , variabel let berisikan nama akhir teman teman , const berisikan umur teman teman saat ini, Setelah itu teman teman tampilkan ketiga variabel tersebut dengan console.log()
+*@Hint : number tanpa petik string dengan petik
+*?4. Happy Codding :)
+ */
+
+var firstName = "Muhammad Fajri";
+let lastName = "Assidiq";
+const age = 25;
+console.log(firstName + lastName + age);
