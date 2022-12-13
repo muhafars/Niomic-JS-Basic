b8f5cf2b0e4ff2f7e69d20b5cdc80672108f12af
muhafash <me.muhfarash@gmail.com>
2022-12-14 03:44:49 +0700

    Tugas 5 Done

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 /**
- ** Tugas 4
+ ** Tugas 
 *? 1. Pastikan Teman Teman Sudah Menginstall Node JS
-*? 2. Teman - Teman Buatlah File di Folder javascript teman teman sebuah file dengan nama file tugas4.js
+*? 2. Teman - Teman Buatlah File di Folder javascript teman teman sebuah file dengan nama file tugas.js
 *? 3. Buatlah Program Dengan Spesifikasi dibawah ini :
+*ToDo 
+- Buatlah sebuah perulangan dengan kondisi <=20 atau sebanyak 20 kali
+*Todo
+- Setelah itu teman teman tampilkan nilai kelipatan 3 pada perulangan tersebut
 *ToDo
-- Buatlah 3 buah variabel dengan yang berisi nilai tinggi badan siswa
-- Setelah itu teman teman gunakanlah fungsi dari if else untuk mengetahui siswa yang memiliki tinggi badan tertinggi
-- Setelah itu teman teman tampilkan hasilnya dari urutan yang tertinggi ke yang terpendek.
+- Tampilkan hasilnya dengan console.log()
 
 *? 4. Happy Codding :)
  */
-
-let tinggi = [180, 160, 170];
-
-let siswaA = tinggi[0];
-let siswaB = tinggi[1];
-let siswaC = tinggi[2];
-
-const setGrade = () => {
-  if (siswaA > siswaB && siswaA > siswaC) {
-    console.log("Siswa A Terbesar");
-  } else if (siswaA < siswaB && siswaA > siswaC) {
-    console.log("Siswa B Terbesar");
-  } else {
-    console.log("Siswa C Terbesar");
+let result;
+for (i = 0; i <= 20; i++) {
+  if (i % 3 === 0 && i > 0) {
+    result = i;
+    console.log(result);
   }
-};
+}
 
-setGrade();
+console.log(`ini dari luar ${result}`); //mengambil output terakhir
